925567750c82da44e9b42ede5c2c63b796fcea9a
wiwilin <katherine.linw@gmail.com>
2019-01-05 17:37:53 +0800

    Initial Commit

---
+++ Instagram_wiwi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram_wiwi
+//
+//  Created by Wei Lin on 2019/1/5.
+//  Copyright © 2019 Wei Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
